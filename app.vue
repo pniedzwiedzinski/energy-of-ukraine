fd4c78a029487481ca04a4eb1bc3ea8ed3200fcf
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-04-10 19:27:32 +0200

    Init setup

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-app>
+      <v-card>
+    <v-layout>
+    <navigation></navigation>
+
+    <v-main style="height: 100vh; min-height: 500px;"></v-main>
+    </v-layout>
+  </v-card>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/navigation.vue';
+
+export default {
+  name: "app",
+  components: {Navigation}
+}
+</script>
+
+<style>
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
---
+++ components/navigation.vue
@@ -0,0 +1,53 @@
+<template>
+
+      <v-navigation-drawer
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://cdn-icons-png.flaticon.com/256/197/197572.png"
+            subtitle=""
+            title="Energy of Ukraine"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-generator-stationary" title="Produkcja" value="generation"></v-list-item>
+          <v-list-item prepend-icon="mdi-connection" title="Zużycie" value="consumption"></v-list-item>
+          <v-list-item prepend-icon="mdi-transmission-tower-export" title="Import/Eksport" value="transfer"></v-list-item>
+          <v-list-item prepend-icon="mdi-currency-eur" title="Ceny" value="prices"></v-list-item>
+          <v-list-item prepend-icon="mdi-barrel" title="Zasoby" value="sources"></v-list-item>
+          <v-list-item prepend-icon="mdi-leaf-circle" title="Środowisko" value="climate"></v-list-item>
+          
+        </v-list>
+
+        <template v-slot:append>
+        <v-divider></v-divider>
+          <div class="pa-2">
+            <v-btn 
+            :icon="arrow"
+            variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+
+</template>
+
+<script>
+export default {
+    props: {
+        rail: false
+    },
+    computed: {
+        arrow() {
+            return this.rail ? "mdi-chevron-left" : "mdi-chevron-right"
+        }
+    }
+}
+</script>
