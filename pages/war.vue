b6260ebdc91cae444dba8b2e5f50d9dbfe102c8c
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-05-13 18:36:59 +0200

    New graphs

--- pages/war.vue
+++ pages/war.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>Wojna</h1>
+    <p>Zarejestrowane straty w sektorze energetycznym</p>
+    <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/a8fdcdca-dcff-405e-876d-65b8c5e28440/page/xewzD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+    <p>Ataki przeprowadzone na infrastrukturę Ukrainy</p>
+    <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/a8fdcdca-dcff-405e-876d-65b8c5e28440/page/p_6b2htwz9gd" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
 </template>
