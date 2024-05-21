067998aa887940659676331328f76a79343f8974
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-05-21 08:41:25 +0200

    Update sources

--- pages/sources.vue
+++ pages/sources.vue
@@ -19,7 +19,31 @@ export default {
                 {
                     "name": "Global Power Plant Database, 2021",
                     "url": "https://datasets.wri.org/dataset/globalpowerplantdatabase",
-                }
+                },
+                {
+                    "name": "IEA World Energy Balances - free extract, 1990-2021",
+                    "url": "https://www.iea.org/data-and-statistics/data-product/world-energy-statistics-and-balances",
+                },
+                {
+                    "name": "Installed electricity production capacity by power plant types, National Power Company Ukrenergo, 2022",
+                    "url": "https://map.ua-energy.org/en/resources/c51a16bc-e990-40db-b790-63624d823daa/",
+                },
+                {
+                    "name": "Electricity prices by type of user [€/kWh] - Eurostat",
+                    "url": "https://ec.europa.eu/eurostat/databrowser/bookmark/5f9b89fe-3871-4d84-a764-b42a4dac9a16?lang=en",
+                },
+                {
+                    "name": "Ukrstat, Fuel reserves archive 2022",
+                    "url": "https://www.ukrstat.gov.ua/operativ/operativ2022/energ/zmp/arh_zmp2022_u.htm",
+                },
+                {
+                    "name": "Damage caused by the energy infrastructure of Ukraine due to the war, Kyiv School of Economics, 2022-2024",
+                    "url": "https://map.ua-energy.org/en/resources/9db4e104-22f3-4e9b-b9f6-0f1f3af49161/",
+                },
+                {
+                    "name": "Information on massive strikes on Ukraine's critical infrastructure, DiXi Group, 2022-2024",
+                    "url": "https://map.ua-energy.org/en/resources/12f3148d-841a-478d-b9ed-72bf0764b286/",
+                },
             ]
         }
     }
