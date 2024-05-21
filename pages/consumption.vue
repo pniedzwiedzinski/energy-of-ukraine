9575654f21a4a6f8153410b139b9c8719e1f3dc4
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-05-21 11:13:10 +0200

    Update: import/eksport

--- pages/consumption.vue
+++ pages/consumption.vue
@@ -2,7 +2,8 @@
     <h1>Zużycie</h1>
 
     <h2>Zużycie energii elektrycznej</h2>
-    <p>Największymi odbiorcami energii elektrycznej są zakłady produkcyjne, gospodarstwa domowe oraz lokale użytkowe. Z racji na </p>
+    <p>Największymi odbiorcami energii elektrycznej są zakłady produkcyjne, gospodarstwa domowe oraz lokale użytkowe.</p>
+    <p>W przeciągu lat 2013-2020 możemy zaobserwować spadek zużycia energii elektrycznej, co może być spowodowane zmniejszającą się liczbą ludności. Dodatkowo, niestabilna sytuacja polityczna nie sprzyja rozwojowi przemysłu. Kiedy zaczął się spadek, miały miejsce takie wydarzenia jak aneksja Krymu czy wojna w Donbasie.</p>
     <figure>
         <img src="~/public/consumption-1.svg">
         <figcaption>Źródło: IEA</figcaption>
--- pages/transfer.vue
+++ pages/transfer.vue
@@ -1,6 +1,20 @@
 <template>
     <h1>Import/Eksport</h1>
+    <p>
+        Celem operatora systemu przesyłowego Ukrainy - Ukrenergo - na ostatnie 10 lat była synchronizacja z systemem Europejskim.
+        Dotychczas Ukraina była zsynchronizowana z rosyjskim systemem IPS/UPS.
+        Zarząd Ukrenergo w raportach rocznych podkreślał to dążenie większą niezależnością i stabilnością systemu. 
+        Ukraina posiada połączenie z systemem europejskim już od 2003 roku w ramach <i>Burshtyn Island</i> - pojedyńcza elektrownia została podpięta pod system ENTSO-E przez połączenie z Węgrami, Słowacją i Rumunią.
+    </p>
+    <p> 
+        W dzień inwazji zaplanowane było testowe odłączenie od systemu rosyjskiego. 
+        Test miał trwać 72 godziny i udowodnić, że sieć ukraińska jest w stanie pracować stabilnie bez synchronizacji z Rosją.
+        Odłączenie okazało się być trwałe, a podłączenie do ENTSO-E stało się naglące.
+        W przeciągu dwóch tygodni udało się spełnić wymagania i osiągnąć cele, a 16 marca 2022 dokonano synchronizacji.
+    </p>
     <p>Przez nadmiar produkcji energii, Ukraina jest eksporterem energii elektrycznej</p>
     <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/3de1db5f-a0fb-4d72-9ca9-5ff94b8570b1/page/eASwD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+    <p>
+Możemy zaobserwować znaczącą przewagę eksportu nad importem. Głównymi importerami ukraińskiego prądu są Węgry i Białoruś, co wynika z dobrze rozwiniętej infrastruktury przesyłowej pomiędzy tymi krajami. Dodatkowo, ciekawą sytuację można zaobserwować w 2015 roku, kiedy to pomimo aneksji Krymu Ukraina importowała prąd z Rosji.</p>
     <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/3de1db5f-a0fb-4d72-9ca9-5ff94b8570b1/page/p_tcu5oqnmgd" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
 </template>
