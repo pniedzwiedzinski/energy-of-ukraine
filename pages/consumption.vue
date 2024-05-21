05dbfbff5e393d4ace89fc4e470f9ea859fcb037
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-05-21 10:49:16 +0200

    Update: war

--- pages/consumption.vue
+++ pages/consumption.vue
@@ -1,15 +1,17 @@
 <template>
     <h1>Zużycie</h1>
 
-    <h2>Zapotrzebowanie energii elektrycznej</h2>
-    <figure>
-        <img src="~/public/consumption-2.svg">
-        <figcaption>Źródło: IEA</figcaption>
-    </figure>
+    <h2>Zużycie energii elektrycznej</h2>
+    <p>Największymi odbiorcami energii elektrycznej są zakłady produkcyjne, gospodarstwa domowe oraz lokale użytkowe. Z racji na </p>
     <figure>
         <img src="~/public/consumption-1.svg">
         <figcaption>Źródło: IEA</figcaption>
     </figure>
+    <figure>
+        <img src="~/public/consumption-2.svg">
+        <figcaption>Źródło: IEA</figcaption>
+    </figure>
+
 
     <h2>Zapotrzebowanie na ciepło</h2>
     <figure>
--- pages/war.vue
+++ pages/war.vue
@@ -1,5 +1,17 @@
 <template>
     <h1>Wojna</h1>
+    <p>Po rozpoczęciu inwazji Rosji na Ukrainę 24 lutego 2022 roku, sektor energetyczny, jako infrstruktura krytyczna państwa, stała się jednym z głównych celów ataków.
+        Sieć elektroenergetyczna była już wcześniej celem <a href="https://media.kasperskycontenthub.com/wp-content/uploads/sites/43/2016/05/20081514/E-ISAC_SANS_Ukraine_DUC_5.pdf">ataku cybernetycznego</a> - wojna między Rosją a Ukrainą rozpoczęła się w 2014 roku.
+        Obecnie straty w sektorze energetycznym szacuje się na prawie 80 miliardów dolarów. 
+    </p>
+    <p>
+        W ramach pomocy powstało wiele inicjatyw, mające na celu zapewnić dostęp do ciepła i prądu mieszkańcom Ukrainy.
+        Aby zapewnić dostęp do energii elektrycznej <a href="https://space-eye.org/generator">wysyłano agregaty prądotwórcze</a>.
+        Powstał również <a href="https://www.energy-community.org/Ukraine/Fund.html">fundusz odbudowy systemu energetycznego</a>, w ramach którego zebrano prawie 400 milionów dolarów - to jednak niewielki procent w porównaniu do odniesionych strat. 
+        Na stronie <a href="https://bids.sciquest.com/apps/Router/PublicEvent?tab=PHX_NAV_SourcingAllOpps&CustomerOrg=MSI&SourcingPublicSite_FilterWorkGroup_PublicSite=%5B%2240341%22%5D&SourcingPublicSite_FILTER_BY_BUSINESS_UNIT=undefined&SourcingPublicSite_FILTER_BY_BUSINESS_UNIT_buDisplayName=&SimpleSearch_Keyword=&tmstmp=1715595394197">
+            bids.sciquest.com</a> można zobaczyć zrealizowane zlecenia.
+
+    </p>
     <p>Zarejestrowane straty w sektorze energetycznym</p>
     <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/a8fdcdca-dcff-405e-876d-65b8c5e28440/page/xewzD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
     <p>Ataki przeprowadzone na infrastrukturę Ukrainy</p>
