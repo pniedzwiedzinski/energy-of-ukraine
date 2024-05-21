1d7be75e3e4ab2c32f4012023d157a038065dbd6
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-05-21 11:30:49 +0200

    Add prices

--- pages/prices.vue
+++ pages/prices.vue
@@ -4,4 +4,6 @@
         <img src="~/public/prices-1.svg">
         <figcaption>Źródło: IEA</figcaption>
     </figure>
+    
+    <iframe width="750" height="422" src="https://lookerstudio.google.com/embed/reporting/d0ca6d35-a081-4419-84fc-6d70ef340dab/page/Bem0D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
 </template>
